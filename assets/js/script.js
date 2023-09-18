7aeebab1921278b1697de10173ccf41da4701c36
JackStras <jackstrasbaugh@yahoo.com>
2023-09-18 16:51:44 -0400

    Created functions for each question, and made it so each one runs when one question is finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,9 @@ var rules = $("#rules")
 var startbtn = $("#start")
 var quiz_section = $("main")
 var timer = $("#timer")
-
+var question_text = $("#text")
+var buttons = $(".answer_button")
+var timeleft = 60
 var questions = [
     {
         question: "What is jQuery used for?",
@@ -47,20 +49,21 @@ var questions = [
     }
 ]
 
-function runquiz() {
 
-}
 
 function endquiz() {
-    timer.text("THIS IS JUST A TEST")
+    var score = timeleft
+    timer.remove()
+    quiz_section.children().remove()
 }
 
 function startquiz(event) {
     title_card.remove()
     rules.remove()
     startbtn.remove()
+    question_text.removeAttr("hidden")
+    buttons.removeAttr("hidden")
 
-    var timeleft = 60
     var timeInterval = setInterval(function() {
         if (timeleft > 0) {
             timer.text("Time: " + timeleft)
@@ -69,17 +72,94 @@ function startquiz(event) {
             clearInterval(timeInterval)
             endquiz()
         }
-    }, 1000)    
+    }, 1000)  
+    question1()
 }
 
 
+function question1(event) {
+    var current = questions[0]
+    question_text.text(current.question)
+    buttons.eq(0).text(current.answers[0])
+    buttons.eq(1).text(current.answers[1])
+    buttons.eq(2).text(current.answers[2])
+    buttons.eq(3).text(current.answers[3])
 
+    buttons.on("click", function(event) {
+        event.preventDefault()
+        if ($(this).text() == current.correct) {
+            buttons.off()
+            question2()
+        } else {
+            timeleft = timeleft - 5
+            buttons.off()
+            question2()
+        }
+    })
+}
 
+function question2() {
+    var current = questions[1]
+    question_text.text(current.question)
+    buttons.eq(0).text(current.answers[0])
+    buttons.eq(1).text(current.answers[1])
+    buttons.eq(2).text(current.answers[2])
+    buttons.eq(3).text(current.answers[3])
 
+    buttons.on("click", function(event) {
+        event.preventDefault()
+        if ($(this).text() == current.correct) {
+            buttons.off()
+            question3()
+        } else {
+            timeleft = timeleft - 5
+            buttons.off()
+            question3()
+        }
+    })
+}
 
+function question3() {
+    var current = questions[2]
+    question_text.text(current.question)
+    buttons.eq(0).text(current.answers[0])
+    buttons.eq(1).text(current.answers[1])
+    buttons.eq(2).text(current.answers[2])
+    buttons.eq(3).text(current.answers[3])
 
+    buttons.on("click", function(event) {
+        event.preventDefault()
+        if ($(this).text() == current.correct) {
+            buttons.off()
+            question4()
+        } else {
+            timeleft = timeleft - 5
+           buttons.off()
+           question4()
+        }
+    })
+}
 
+function question4() {
+    var current = questions[3]
+    question_text.text(current.question)
+    buttons.eq(0).text(current.answers[0])
+    buttons.eq(1).text(current.answers[1])
+    buttons.eq(2).text(current.answers[2])
+    buttons.eq(3).text(current.answers[3])
 
+    buttons.on("click", function(event) {
+        event.preventDefault()
+        if ($(this).text() == current.correct) {
+            buttons.off()
+            endquiz()
+        } else {
+            timeleft = timeleft - 5
+            buttons.off()
+            endquiz()
+        }
+    })
+}
 
 
 
